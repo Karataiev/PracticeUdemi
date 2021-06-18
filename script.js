8aabfcf0bc395695b2e1725610da95659e4a840f
IgorKarataiev <Karataiev.pro@gmail.com>
2021-06-18 12:06:48 +0300

    Practice_1

--- script.js
+++ script.js
@@ -28,4 +28,22 @@
 // console.log(decr--);
 // console.log(5%2);
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+ const a = prompt('Один из последних просмотреный фильмов', ''),
+       b = prompt('На сколько оцените его?', ''),
+       c = prompt('Один из последних просмотреный фильмов', ''),
+       d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
